bd1833cf2d91d3d55af4397136460cf8a0cfc5fb
Thomas <9749173+uhthomas@users.noreply.github.com>
2019-05-18 22:15:09 +0100

    remove test code from go

--- go/go.go
+++ go/go.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var j int
 	const s = "ksqu"
 	for i := 0; ; i = (i + 1) % 2 {
-		if j++; j > 9 {
-			return
-		}
 		fmt.Printf("You're a %sid now!\n", s[i:1+i*3])
 	}
 }
